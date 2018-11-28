ddc21f9b36515ed11264a35bcb5556bdb1a4b4fe
Dillon Niederhut <dillon.niederhut@gmail.com>
2018-11-28 12:47:48 -0700

    REL: bump versions to 0.1.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2018, Dillon Niederhut'
 author = 'Dillon Niederhut'
 
 # The short X.Y version
-version = '0.1.0'
+version = '0.1.1'
 # The full version, including alpha/beta/rc tags
 release = ''
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='niacin',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     package_data={
         'niacin': ['data/*']
