e0e7b703548ce0d8df1e39702ecb51a238d33331
Dillon Niederhut <dillon.niederhut@gmail.com>
2020-08-24 07:59:04 -0500

    REL: release v0.4.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Dillon Niederhut'
 author = 'Dillon Niederhut'
 
 # The short X.Y version
-version = '0.2.0'
+version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
